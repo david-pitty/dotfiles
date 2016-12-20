36aaffbb657fc20f62f94ea3c1b62abb691adb7f
David Pitty <dpitty13@gmail.com>
2016-12-19 18:56:08 -0600

    added install.sh file with commands to source dotfiles

---
+++ install.sh
@@ -0,0 +1,11 @@
+echo "source $(pwd)/shell_sources/bashrc" >> ~/.bashrc
+echo "source $(pwd)/shell_sources/aliases" >> ~/.bashrc
+echo "source $(pwd)/shell_sources/git_commands" >> ~/.bashrc
+
+echo "source $(pwd)/shell_sources/git_commands" >> ~/.zshrc
+echo "source $(pwd)/shell_sources/aliases" >> ~/.zshrc
+
+echo ":so $(pwd)/vimrc" >> ~/.vimrc
+echo "source-file $(pwd)/tmux.conf" >> ~/.tmux.conf
+
+git config --global include.path "$(pwd)/gitcofig"
