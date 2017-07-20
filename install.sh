313c2fa08d83b01a3614694a8397ce04d8e9dda2
David Pitty <david.pitty.ajun@intel.com>
2017-07-20 10:50:50 -0600

    updated install.sh file

--- install.sh
+++ install.sh
@@ -6,7 +6,10 @@ echo "source $(pwd)/shell_sources/zshrc" >> ~/.zshrc
 echo "source $(pwd)/shell_sources/git_commands" >> ~/.zshrc
 echo "source $(pwd)/shell_sources/aliases" >> ~/.zshrc
 
-echo ":so $(pwd)/vimrc" >> ~/.vimrc
+echo ":so $(pwd)/vim/plugins.vim" >> ~/.vimrc
+echo ":so $(pwd)/vim/vimrc" >> ~/.vimrc
+echo ":so $(pwd)/vim/keybinding.vim" >> ~/.vimrc
+
 echo "source-file $(pwd)/tmux.conf" >> ~/.tmux.conf
 
 cat ./minttyrc > ~/.minttyrc
