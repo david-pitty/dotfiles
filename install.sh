1fa1bc461773d2685bd9b180417e9f6eeebfebe9
David Pitty Ajun <dpitty13@gmail.com>
2017-08-05 20:44:36 -0600

    adding keybings for Mac on inputrc

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ echo "source $(pwd)/shell_sources/zshrc" >> ~/.zshrc
 echo "source $(pwd)/shell_sources/git_commands" >> ~/.zshrc
 echo "source $(pwd)/shell_sources/aliases" >> ~/.zshrc
 
-echo "set completion-ignore-case On" >> ~/.inputrc
+echo "\$include $(pwd)/inputrc" >> ~/.inputrc
 
 echo ":so $(pwd)/vim/plugins.vim" >> ~/.vimrc
 echo ":so $(pwd)/vim/vimrc" >> ~/.vimrc
