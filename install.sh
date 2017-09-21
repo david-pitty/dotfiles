cd21de21dca4f3c5f1224b9a51e1dadd873caafd
David Pitty <david.pitty.ajun@intel.com>
2017-09-21 10:56:27 -0600

    added comments for other vim settings

--- install.sh
+++ install.sh
@@ -3,17 +3,21 @@ echo "if [ -f ~/.bashrc ]; then . ~/.bashrc; fi" > ~/.bash_profile
 echo "source $(pwd)/shell_sources/bashrc" >> ~/.bashrc
 echo "source $(pwd)/shell_sources/aliases" >> ~/.bashrc
 echo "source $(pwd)/shell_sources/git_commands" >> ~/.bashrc
-echo "source $(pwd)/shell_sources/bash_prompt" >> ~/.bashrc
+#echo "source $(pwd)/shell_sources/bash_prompt" >> ~/.bashrc
 
-echo "source $(pwd)/shell_sources/zshrc" >> ~/.zshrc
-echo "source $(pwd)/shell_sources/git_commands" >> ~/.zshrc
-echo "source $(pwd)/shell_sources/aliases" >> ~/.zshrc
+#echo "source $(pwd)/shell_sources/zshrc" >> ~/.zshrc
+#echo "source $(pwd)/shell_sources/git_commands" >> ~/.zshrc
+#echo "source $(pwd)/shell_sources/aliases" >> ~/.zshrc
 
 echo "\$include $(pwd)/inputrc" >> ~/.inputrc
 
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo ":so $(pwd)/vim/plugins.vim" >> ~/.vimrc
 echo ":so $(pwd)/vim/vimrc" >> ~/.vimrc
 echo ":so $(pwd)/vim/keybinding.vim" >> ~/.vimrc
+#echo ":so $(pwd)/vim/powershell.vim" >> ~/.vimrc
+#echo ":so $(pwd)/vim/javascript.vim" >> ~/.vimrc
+#echo ":so $(pwd)/vim/python.vim" >> ~/.vimrc
 
 echo "source-file $(pwd)/tmux.conf" >> ~/.tmux.conf
 
