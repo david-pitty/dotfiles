4c62ff42d133edaa12f7051b4aa293c9b6288934
David Pitty <david_pitty@qat.com>
2020-04-28 08:17:02 -0600

    adding ssh agent configuration

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
+unalias echo
 echo "if [ -f ~/.bashrc ]; then . ~/.bashrc; fi" > ~/.bash_profile
+#echo "source $(pwd)/shell_sources/bash_profile_ssh_agent" >> ~/.bash_profile
 
 echo "source $(pwd)/shell_sources/bashrc" >> ~/.bashrc
 echo "source $(pwd)/shell_sources/aliases.sh" >> ~/.bashrc
@@ -11,12 +13,12 @@ echo "source $(pwd)/shell_sources/bash_prompt.sh" >> ~/.bashrc
 
 echo "\$include $(pwd)/inputrc" >> ~/.inputrc
 
-#curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo ":so $(pwd)/vim/vimrc" >> ~/.vimrc
 
 echo "source-file $(pwd)/tmux.conf" >> ~/.tmux.conf
 
-cat ./minttyrc > ~/.minttyrc
+# cat ./minttyrc > ~/.minttyrc
 
 git config --global include.path "$(pwd)/gitcofig"
 git config --global core.excludesfile "$(pwd)/gitignore_global"
