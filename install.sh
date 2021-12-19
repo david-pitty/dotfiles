bd1065c053bc9cf9e111d8751d4b0f37a7637cef
David Pitty <dpitty13@gmail.com>
2021-12-18 19:23:03 -0600

    adding neovim files

--- install.sh
+++ install.sh
@@ -1,28 +1,38 @@
 unalias echo
-echo "if [ -f ~/.bashrc ]; then . ~/.bashrc; fi" > ~/.bash_profile
-#echo "source $(pwd)/shell_sources/bash_profile_ssh_agent" >> ~/.bash_profile
 
+# BASH
+# echo "if [ -f ~/.bashrc ]; then . ~/.bashrc; fi" > ~/.bash_profile
 echo "source $(pwd)/shell_sources/bashrc" >> ~/.bashrc
 echo "source $(pwd)/shell_sources/aliases.sh" >> ~/.bashrc
 echo "source $(pwd)/shell_sources/git_commands.sh" >> ~/.bashrc
 echo "source $(pwd)/shell_sources/bash_prompt.sh" >> ~/.bashrc
 
-#echo "source $(pwd)/shell_sources/zshrc" >> ~/.zshrc
-#echo "source $(pwd)/shell_sources/git_commands.sh" >> ~/.zshrc
-#echo "source $(pwd)/shell_sources/aliases.sh" >> ~/.zshrc
+# ZSH
+# echo "source $(pwd)/shell_sources/zshrc" >> ~/.zshrc
+# echo "source $(pwd)/shell_sources/git_commands.sh" >> ~/.zshrc
+# echo "source $(pwd)/shell_sources/aliases.sh" >> ~/.zshrc
 
+# linux inputrc
 echo "\$include $(pwd)/inputrc" >> ~/.inputrc
 
+# vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 echo ":so $(pwd)/vim/vimrc" >> ~/.vimrc
 
+# TMUX
 echo "source-file $(pwd)/tmux.conf" >> ~/.tmux.conf
 
+# git bash for windows
 # cat ./minttyrc > ~/.minttyrc
 
+# GIT
 git config --global include.path "$(pwd)/gitcofig"
 git config --global core.excludesfile "$(pwd)/gitignore_global"
 
-# Visual Studio Code
+# VSCODE
 # ln -s vscode/keybindings.json ~/AppData/Roaming/Code/User/keybindings.json
 # ln -s vscode/settings.json ~/AppData/Roaming/Code/User/settings.json
+
+# NEOVIM
+mkdir -p ~/.config/nvim
+echo "execute 'source' '$(pwd)/neovim/init.vim'"
---
+++ neovim/init.vim
@@ -0,0 +1,8 @@
+syntax enable
+set number
+set relativenumber
+set ignorecase
+set smartcase
+set showcmd
+
+let mapleader = " "
