9f9f749b2e27d150a49a4e1d5d8c60a8203d733f
David Pitty Ajun <david.pitty.ajun@intel.com>
2022-01-19 20:10:03 -0600

    updated bash prompt

--- shell_sources/bash_prompt.sh
+++ shell_sources/bash_prompt.sh
@@ -1,4 +1,4 @@
 parse_git_branch() {
-     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ (\1)/'
+     echo " ($(git rev-parse --abbrev-ref HEAD 2> /dev/null))"
 }
 export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[35m\]\$(parse_git_branch)\[\033[m\]\$ "
