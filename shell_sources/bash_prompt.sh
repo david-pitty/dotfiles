5d1025c36c41cd85bdc7cf7074dec24cab94128d
David Pitty Ajun <david.pitty.ajun@intel.com>
2022-05-29 15:44:36 -0600

    corrected bash prompt

--- shell_sources/bash_prompt.sh
+++ shell_sources/bash_prompt.sh
@@ -1,4 +1,8 @@
 parse_git_branch() {
-     echo " ($(git rev-parse --abbrev-ref HEAD 2> /dev/null))"
+    git_branch=$(git rev-parse --abbrev-ref HEAD 2> /dev/null)
+    if [ $? -eq 0 ]
+    then
+        echo " ($git_branch)"
+    fi
 }
 export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[35m\]\$(parse_git_branch)\[\033[m\]\$ "
