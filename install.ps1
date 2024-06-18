5d7fcdbfd264ea92c2bd02e3c6e4cb9fb0683a09
david-pitty <dpitty13@gmail.com>
2024-06-17 20:56:53 -0600

    added more info on install.ps1

--- install.ps1
+++ install.ps1
@@ -2,7 +2,12 @@
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 
+# scoop
 scoop bucket add nerd-fonts
 scoop install git jq neovim erd busybox nerd-fonts/Hack-NF-Mono
 
+# git
 git config --global include.path "$(pwd)/gitconfig"
+
+# nvim
+Copy-Item -Recurse -Force nvim\ ~\AppData\Local\
