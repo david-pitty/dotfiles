3ee12e5bff643888a75ba8f30eacbde74e20f9b8
david-pitty <dpitty13@gmail.com>
2024-06-29 11:08:33 -0600

    remove pass

--- install-packages.sh
+++ install-packages.sh
@@ -2,4 +2,4 @@ sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo apt update
 sudo apt upgrade
 
-sudo apt install tree tmux build-essential gcc neovim neofetch unzip pass
+sudo apt install tree tmux build-essential gcc neovim neofetch unzip
