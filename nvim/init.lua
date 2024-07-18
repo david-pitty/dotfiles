a3c7735d486af06438591e213c25b006a78b765f
david-pitty <dpitty13@gmail.com>
2024-07-17 21:47:40 -0600

    added scrolloff to keep context

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,6 +8,8 @@ vim.opt.relativenumber = true
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.scrolloff = 10
+
 vim.cmd('filetype plugin on')
 vim.opt.omnifunc = 'syntaxcomplete#Complete'
 vim.opt.tags = 'tags'
