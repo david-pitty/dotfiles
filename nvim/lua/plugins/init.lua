5cca7e1c5678e4b0b5d0bd1180d12fe7086f40bd
Pitty Ajun, David <david.pitty.ajun@intel.com>
2025-05-08 13:04:58 -0600

    fixed colorscheme

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,6 +1,8 @@
+require('plugins.colorscheme')
+
 require('lualine').setup()
 require('bufferline').setup()
 require('nvim-tree').setup()
 -- require('plugins.treesitter')
-require('plugins.lsp')
+-- require('plugins.lsp')
 
